59eb0ee325f22be686e83f0491fe3ba8da5da155
huqiaoli <huqiaoli@jz-sh.cn>
2023-04-07 15:06:49 +0800

    fix: 修改姓名手机号查询时间格式设置

--- src/pages/EmployeeProfile/index.tsx
+++ src/pages/EmployeeProfile/index.tsx
@@ -3,7 +3,6 @@ import { useDispatch, Dispatch } from '@kkt/pro';
 import { Card, Tabs } from 'uiw';
 import Search from './Search';
 import Table from './Table';
-import Modals from './Modals'
 
 const Page = () => {
   const dispatch = useDispatch<Dispatch>();
@@ -16,10 +15,7 @@ const Page = () => {
       <Tabs className='tabsRecord' type="line" activeKey="1" onTabClick={(key, tab, e) => {
         console.log("=>", key, tab);
       }}>
-        <Tabs.Pane label="人员信息档案" key="1">
-          <Modals />
-        </Tabs.Pane>
-        <Tabs.Pane label="人员信息档案记录" key="2">
+        <Tabs.Pane label="人员信息档案记录" key="1">
           <Card noHover bordered={false} style={{ marginBottom: 14 }}><Search /></Card>
           <Card noHover bordered={false}><Table /></Card>
         </Tabs.Pane>
