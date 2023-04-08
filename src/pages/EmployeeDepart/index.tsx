638dd9f58a75e417c1774b42e183ec52152ebdd5
huqiaoli <huqiaoli@jz-sh.cn>
2023-04-08 13:31:25 +0800

    fix: 修改离职管理查询列表

--- src/pages/EmployeeDepart/index.tsx
+++ src/pages/EmployeeDepart/index.tsx
@@ -1,14 +1,13 @@
 import { Fragment } from 'react';
-import { Divider } from 'uiw';
+import { Card } from 'uiw';
 import Search from './Search';
 import Table from './Table';
 
 const Page = () => {
   return (
     <Fragment>
-      <Search />
-      <Divider />
-      <Table />
+      <Card noHover bordered={false} style={{ marginBottom: 14 }}><Search /></Card>
+      <Card noHover bordered={false}><Table /></Card>
     </Fragment>
   )
 }
