bf03666752e0cf840aa9972be9ff09dd3c60bcb1
rulishu <1324214367@qq.com>
2023-04-18 18:00:05 +0800

    fix(登录页面): 调用获取第三方token和第三方登录接口

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,33 +1,89 @@
-import { useEffect } from 'react';
+import { useEffect, Fragment } from 'react';
 import { useDispatch, Dispatch, KktproPageProps, useLocation } from '@kkt/pro';
 import UserLogin from "@uiw-admin/user-login";
 import { Notify } from 'uiw';
 
 const Pages = ({ navigate }: KktproPageProps) => {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
   const dispatch = useDispatch<Dispatch>();
 
+  const thirdLogin = () => {
+    dispatch({ type: 'global/fetchThirdLoginToken' });
+  }
+
   useEffect(() => {
+    if (search) {
+      const code = search.split('code=')[1];
+      dispatch({
+        type: 'global/thirdLogin',
+        payload: {
+          code,
+          callback: () => {
+            navigate('/home');
+          },
+        },
+      });
+    }
     localStorage.removeItem("token");
-  }, [pathname])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname, dispatch])
 
   return (
-    <UserLogin
-      api="/api/login"
-      projectName="人事管理系统"
-      btnProps={{ type: "primary" }}
-      onSuccess={(data) => {
-        if (data.code === 200 && data.token) {
-          Notify.success({ description: '登录成功' });
-          localStorage.setItem("token", data?.token);
-          dispatch.global.getUserInfo({
-            callback: (authRoutes: string[]) => {
-              navigate('/home', { replace: true })
-            }
-          });
-        }
-      }}
-    />
+    <Fragment>
+      {/* <UserLogin
+        api="/api/login"
+        projectName="人事管理系统"
+        btnProps={{ type: "primary" }}
+        onSuccess={(data) => {
+          if (data.code === 200 && data.token) {
+            Notify.success({ description: '登录成功' });
+            localStorage.setItem("token", data?.token);
+            dispatch.global.getUserInfo({
+              callback: (authRoutes: string[]) => {
+                navigate('/home', { replace: true })
+              }
+            });
+          }
+        }}
+      /> */}
+      <UserLogin
+        api="/api/login"
+        projectName="人事管理系统"
+        buttons={[
+          {
+            title: "登录",
+            htmlType: "submit",
+            type: "primary"
+          },
+          {
+            title: "第三方登陆",
+            basic: true,
+            icon: "github-o",
+            type: "link",
+            onClick: () => { thirdLogin() }
+          },
+        ]}
+        onSuccess={(data) => {
+          if (data.code === 200 && data.token) {
+            Notify.success({ description: '登录成功' });
+            localStorage.setItem("token", data?.token);
+            dispatch.global.getUserInfo({
+              callback: (authRoutes: string[]) => {
+                navigate('/home', { replace: true })
+              }
+            });
+          }
+        }}
+      />
+      {/* <Loader
+        tip="第三方登录加载中..."
+        size="large"
+        bgColor="rgba(255, 255, 255, 0.9)"
+        fullscreen={isFullscreen}
+        loading={isFullscreen}
+      /> */}
+
+    </Fragment>
   )
 };
 export default Pages;
