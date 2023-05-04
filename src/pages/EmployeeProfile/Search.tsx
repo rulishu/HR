d45aaa054aa75a98eb04c2d1df85715be7c68cd8
panbibi <1265629139@qq.com>
2023-05-04 17:29:42 +0800

    fix:档案查询条件优化和@kkt/pro升级 #12

--- src/pages/EmployeeProfile/Search.tsx
+++ src/pages/EmployeeProfile/Search.tsx
@@ -1,21 +1,23 @@
 import { useDispatch, Dispatch } from '@kkt/pro';
-import { ProForm } from "@uiw-admin/components";
-import formatter from "@uiw/formatter";
+import { ProForm } from '@uiw-admin/components';
+import formatter from '@uiw/formatter';
+import { DateInputRange } from 'uiw';
+import './style/index.css';
 
 //搜索表单
 const formSearchList = [
   {
-    label: "姓名｜手机号",
-    key: "search",
-    widget: "input",
-    placeholder: "请输入姓名或手机号"
+    label: '姓名｜手机号',
+    key: 'search',
+    widget: 'input',
+    placeholder: '请输入姓名或手机号',
   },
   {
-    label: "入职日期",
-    key: "entryDateStart",
-    widget: "dateInput",
+    label: '入职日期',
+    key: 'entryDateStart',
+    widget: 'dateInputRange',
     widgetProps: {
-      format: 'YYYY-MM-DD'
+      format: 'YYYY-MM-DD',
     },
   },
   // {
@@ -36,14 +38,14 @@ const formSearchList = [
   //   ],
   // },
   {
-    label: "员工状态",
-    key: "probation",
-    widget: "select",
+    label: '员工状态',
+    key: 'probation',
+    widget: 'select',
     option: [
-      { value: 0, label: "正式" },
-      { value: 1, label: "实习" },
-      { value: 2, label: "试用" },
-      { value: 3, label: "已离职" },
+      { value: 0, label: '正式' },
+      { value: 1, label: '实习' },
+      { value: 2, label: '试用' },
+      { value: 3, label: '已离职' },
     ],
   },
 ];
@@ -54,33 +56,36 @@ const Search = () => {
   const onScreenSubmit = (current?: any) => {
     dispatch.employeeProfile.selectStaffFile({
       ...current,
-      entryDateStart: current?.entryDateStart && formatter(
-        "YYYY-MM-DD",
-        new Date(current?.entryDateStart)
-      ),
+      entryDateStart:
+        current?.entryDateStart?.at(0) && formatter('YYYY-MM-DD', new Date(current?.entryDateStart?.at(0))),
+      entryDateEnd: current?.entryDateStart?.at(1) && formatter('YYYY-MM-DD', new Date(current?.entryDateStart?.at(1))),
       // entryDateEnd: current?.entryDateEnd && formatter(
       //   "YYYY-MM-DD",
       //   new Date(current?.entryDateEnd)
       // ),
-      page: 1
+      page: 1,
     });
-  }
+  };
   return (
     <ProForm
+      className="proForms"
       formType="pure"
       showSaveButton
       showResetButton
       saveButtonProps={{
-        type: "primary",
-        label: '搜索'
+        type: 'primary',
+        label: '搜索',
       }}
       cardProps={{
         noHover: true,
       }}
       onSubmit={(_, current) => onScreenSubmit(current)}
       formDatas={formSearchList}
+      customWidgetsList={{
+        dateInputRange: DateInputRange,
+      }}
     />
-  )
-}
+  );
+};
 
 export default Search;
